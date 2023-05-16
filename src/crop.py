845f043abdbc7c1b155694feeb0b19efa7c3336d
marcelotores <marcelotores21@gmail.com>
2023-05-16 17:06:44 -0300

    corta imagem

--- src/crop.py
+++ src/crop.py
@@ -5,9 +5,9 @@ from src import utilidades as ut
 import cv2 as cv
 
 
-def corta(img, coordenadas):
+def corta(im, left, top, right, bottom):
 
-    im1 = im.crop((coordenadas[0], coordenadas[1], coordenadas[2], coordenadas[3]))
+    im1 = im.crop((left, top, right, bottom))
     im1.show()
 
 # Opens a image in RGB mode
@@ -17,15 +17,34 @@ img = cv.imread("../imagens/1.jpg")
 # Size of the image in pixels (size of original image)
 # (This is not mandatory)
 width, height = im.size
+#print(int((height-88.75)/20))
 
 
-for left in range(0, width, 20):
-    for top in range(0, height, 20):
-        print(left, top)
-        print(left+88.75, top+88.75)
-        print()
+#for left in range(0, width, 20):
+#    print(left)
+
+
+patch_width = 350
+patch_height = 300
+aument = 70
+for left in range(0, width, aument):
+    right = left + patch_width
+    if right > width:
+        break
+    for top in range(0, height, aument):
+        bottom = top + patch_height
+        if bottom > height:
+            break
+        corta(im, left, top, right, bottom)
+        print(left, top, right, bottom)
+
     #corta(im, (0, 0, 88.75, 88.75))
 print(width, height)
+
+
+
+
+
 #cv.imshow('', img)
 # Shows the image in image viewer
 #im1.show()
