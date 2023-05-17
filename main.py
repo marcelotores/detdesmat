bcd6d791c243b7ee275c382cdfa8e4928af439ca
marcelotores <marcelotores21@gmail.com>
2023-05-17 18:16:03 -0300

    nada

--- main.py
+++ main.py
@@ -7,21 +7,6 @@ from PIL import Image
 img1 = cv.imread('imagens/p5.jpg')
 img2 = cv.imread('imagens/1.jpg')
 
-imagem_original_pil = Image.open(r"imagens/1.jpg")
-imagem_original_pil_num = np.array(imagem_original_pil)
-imagem_original_numpy = cv.imread("imagens/1.jpg")
-
-kp1_pil_num, des1_pil_num = sift.sift_detectores_e_descritores(imagem_original_pil_num, 10)
-kp1_num, des1_num = sift.sift_detectores_e_descritores(imagem_original_numpy, 10)
-print(len(kp1_pil_num))
-print(len(kp1_num))
-
-
-img1=cv.drawKeypoints(imagem_original_pil_num,kp1_pil_num,imagem_original_pil_num)
-img2=cv.drawKeypoints(imagem_original_numpy,kp1_num,imagem_original_numpy)
-cv.imshow(f'pil{len(kp1_pil_num)}', img1)
-cv.imshow(f'num{len(kp1_num)}', img2)
-cv.waitKey(0)
 
 kp1, des1 = sift.sift_detectores_e_descritores(img1)
 kp2, des2 = sift.sift_detectores_e_descritores(img2)
@@ -29,65 +14,4 @@ kp2, des2 = sift.sift_detectores_e_descritores(img2)
 
 imagem_out, good, kp1, kp2 = sift.correspondencias(img1, img2, kp1=kp1, kp2=kp2,
                                                    des1=des1, des2=des2)
-#print(correspondencias)
-#for g, n in correspondencias:
-#    print(g, n)
-#print(good)
-#print(good[1][0].distance)
-#print(good[1][0].imgIdx)
-#print(good[1][0].queryIdx)
-#print(good[1][0].trainIdx)
-#print()
-#print(good[0][0].distance)
-#print(good[0][0].imgIdx)
-#print(good[0][0].queryIdx)
-#print(good[0][0].trainIdx)
-#print(dir(kp1[0]))
-
-#for g in good:
-#    pass
-     #print(kp1[g[0].queryIdx].pt, kp2[g[0].trainIdx].pt, g[0].distance)
-
-
-#for m, n in correspondencias:
-#    print(m, n, m.distance, n.distance)
-    #print('m')
-    #print(kp1[m.queryIdx].pt, kp2[m.trainIdx].pt, m.distance)
-    #print('n')
-    #print(kp1[n.queryIdx].pt, kp2[n.trainIdx].pt, n.distance)
-#print(correspondencias)
-    #print(m, n)
-#for g in good:
-#    print(g)
-#print('Pontos 1')
-#for k1 in kp1:
-#    print(k1)
-#print('Pontos 2')
-#for k2 in kp2:
-#    print(k2)
-
-#p1 = kp1[good[0][0].queryIdx]
-#p2 = kp2[good[0][0].trainIdx]
-#print(p1, p2)
-#for k in kp1:
-#    print(k)
-#for k2 in kp2:
-#    print(k2.pt)
-#print(correspondencias)
-
-
-
-#for match in matches:
-#  p1 = kp1[match.queryIdx].pt
-#  p2 = kp2[match.trainIdx].pt
-#print(kp1[0])
-#print(kp2[0])
-#cv.imshow(f'{len(kp1)}:{len(kp2)} -- {len(good)}', imagem_out)
-#cv.waitKey(0)
-#plt.imshow(imagem_out)
-#plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-#plt.show()
-
-
-
 
