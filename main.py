d1d553c4165b2d77e93d98a3a08a6f30e2acd273
marcelotores <marcelotores21@gmail.com>
2023-05-19 17:07:26 -0300

    Inicio de gráfico

--- main.py
+++ main.py
@@ -18,26 +18,7 @@ imagem_out, good, kp1, kp2 = sift.correspondencias(img1, img2, kp1=kp1, kp2=kp2,
 # for g in good:
 #     print(g[0].distance)
 
-good_np = np.array(good)
-good_np = good_np.reshape(good_np.shape[0], )
-arr_distance = np.array([])
-#app_arr = np.append(arr, [13,15,17])
-count_0 = 0
-for g in good_np:
-    arr_distance = np.append(arr_distance, g.distance)
-    if g == 0:
-        count_0 += 1
 
-print(np.min(arr_distance))
-print(np.max(arr_distance))
-print(np.average(arr_distance))
-print(count_0)
-print('----')
-print(len(kp1))
-print(len(kp2))
-menor_ponto = (min(len(kp1), len(kp2)))
-print(menor_ponto)
-
-print(good_np.shape[0]/menor_ponto)
-
-
+imagem = cv.imread('src/teste.jpg')
+print(imagem.shape)
+print(img2.shape)
--- src/crop.py
+++ src/crop.py
@@ -17,6 +17,8 @@ patch_width = 85
 patch_height = 85
 aument = 20
 count = 0
+rep = []
+goods = []
 for left in range(0, width, aument):
     right = left + patch_width
     if right > width:
@@ -40,31 +42,53 @@ for left in range(0, width, aument):
                                                                              des1=des1, des2=des2)
         print('##################################################################')
         print(f'#### Patch {count} ---> {len(kp1)}:{len(kp2)} -- {len(good)} ####')
+        goods.append(len(good))
+        # with open("hello.txt", "a") as my_file:
+        #     my_file.write('###################################################################\n')
+        #     my_file.write(f'#### Patch {count} ---> {len(kp1)}:{len(kp2)} -- {len(good)} ####\n')
 
         ## Calculo da distancia mínima, máxima e média
         if len(good) != 0:
             good_np = np.array(good)
             good_np = good_np.reshape(good_np.shape[0], )
             arr_distance = np.array([])
+            repetitibilidade = good_np.shape[0] / (min(len(kp1), len(kp2)))
+            rep.append(repetitibilidade)
             # app_arr = np.append(arr, [13,15,17])
             count_0 = 0
             for g in good_np:
                 arr_distance = np.append(arr_distance, g.distance)
                 if g.distance == 0:
                     count_0 += 1
-            print('Repetibilidade (matching/min[qtd_kp1, qtd_kp2]): ', good_np.shape[0]/(min(len(kp1), len(kp2))))
+
+
+            print('Repetibilidade (matching/min[qtd_kp1, qtd_kp2]): ', repetitibilidade)
             print('Min: ', np.min(arr_distance))
             print('Máx: ', np.max(arr_distance))
             print('Méd: ', np.average(arr_distance))
             print('Quantidade de correspondências Zeros: ', count_0)
-
-        #patch.save(f"../imagens/patchs/Patch {count}.jpg")
+            # with open("hello.txt", "a") as my_file:
+            #     my_file.write(f'Repetibilidade (matching/min[qtd_kp1, qtd_kp2]): {repetitibilidade}\n')
+            #     my_file.write(f'Min: {np.min(arr_distance)}\n')
+            #     my_file.write(f'Máx: {np.max(arr_distance)}\n')
+            #     my_file.write(f'Méd: {np.average(arr_distance)}\n')
+            #     my_file.write(f'Quantidade de correspondências Zeros: {count_0}\n')
+        #Image.fromarray(imagem_out).save(f"../imagens/patchs/Patch {count}.jpg")
         count+=1
 
+#cv.imshow(f'{len(kp1)}:{len(kp2)} -- {len(good)}', imagem_out)
+#cv.waitKey(0)
 
-cv.imshow(f'{len(kp1)}:{len(kp2)} -- {len(good)}', imagem_out)
-cv.waitKey(0)
 
+
+media = sum(rep) / len(rep)
+print('Media Rep: ', media)
+print(goods)
+plt.plot(goods)
+plt.title('Quantidade de Correspondências por Imagem')
+plt.xlabel('Imagem')
+plt.ylabel('Qtd. de Correspondências')
+plt.show()
 #plt.imshow(img)
 #plt.imshow(img2)
 #plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
